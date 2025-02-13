ac8e0c80a1576704776d0e4bc00ad9d11c816a07
Cordagr <113870656+Cordagr@users.noreply.github.com>
2025-02-13 11:25:13 -0600

    Update server.js
    Implementing post ACR cloud call

--- server.js
+++ server.js
@@ -2,13 +2,54 @@ const express = require("express")
 const multer= require("multer")
 const axios = require("axios")
 const fs = require("fs")
-
+const FormData = require("form-data")
+const crypto = require("crypto")
 const app = express()
 const upload = multer({dest:"uploads/})
+const ACR_HOST = "https://idenity-us-west-2.acrcloud.com/v1/identify"; // Region based replace if diffent
 
-app.post("/recognize", upload.single("file"), async(req,res) =>
-  {
-    const filePath = req.file.path
-  }
+function generateSignedSignature(secret, timestamp)
+{
+  return crypto.createHmax("sha1",secret).update(timestamp.toString()).digest("base64")
+}
+
+async function recognizeSong(filePath)
+{
+const form = new FormData()
+form.append("sample", fs.createReadStream(filepath))
+form.append("access_key",process.env.ACR_ACCESS_SECRET)
+form.append("data_type","audio")
+form.append(signature_version","1")
+
+const timestamp = Math.floor(Data.now()/1000)
+const signature = generateSignature(process.env.ACR_ACCESS_SECRET, timestamp)
+form.append("signature",signature)
+form.append("timestamp",timestamp)
+
+try
+{
+  const response = await axios.post(ACR_HOST, form, {
+  headers: form.getHeaders(),
+})
+  return response.data.metadata?.music[0] || null
+} catch(error)
+{
+  console.error('ACRCloud Error:, error.reponse?.data || error.message)
+  return null
+}
+
+
+
+
+
+
+
+
+
+
+
+  
+}
+
 
         
